bfa8cfe5aa362218077b1f8424fe36f4609d8cd3
ahmadbacchus <abacchus@outlook.com>
2022-11-15 19:48:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ module "MY-VM-MODULE" {
     source = "./modules/aws"
 
     instance_type          = var.instance_type
+    key_name               = var.key_name
     vpc_security_group_ids = var.vpc_security_group_ids
     subnet_id = var.subnet_id
     ami                    = var.ami
